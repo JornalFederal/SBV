85ad4a6380049da531cefba6afba3cfc56d09b5d
Jornal Federal <jornalfederal.ifsp.sbv@gmail.com>
2024-09-21 18:21:20 -0300

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,6 +1,27 @@
-// Precisa de Servidor e PHP para funcionar.
-document.getElementById('sugestaoForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário
-    // Limpa o formulário
-    document.getElementById('sugestaoForm').reset();
+document.addEventListener('DOMContentLoaded', function() {
+    // Agora o código só será executado após o carregamento completo do DOM
+
+    // Seleciona a miniatura do vídeo e o botão de fechar
+    const videoThumbnail = document.getElementById('videoThumbnail');
+    const videoModal = document.getElementById('videoModal');
+    const videoFrame = document.getElementById('videoFrame');
+    const closeModalButton = document.getElementById('closeModal');
+
+    // Função para abrir o modal e carregar o vídeo
+    function openModal() {
+        console.log('Abrindo modal...');
+        videoModal.style.display = 'block';
+    }
+
+    // Função para fechar o modal e parar o vídeo
+    function closeModal() {
+        console.log('Fechando modal...');
+        videoModal.style.display = 'none';
+    }
+
+    // Adiciona o evento de clique na miniatura do vídeo
+    videoThumbnail.addEventListener('click', openModal);
+
+    // Adiciona o evento de clique no botão de fechar
+    closeModalButton.addEventListener('click', closeModal);
 });
