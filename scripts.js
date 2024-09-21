0f281cb620ac3eea221505202e6d64712b742c9e
unknown <jornalfederal.ifsp.sbv@gmail.com>
2024-09-21 19:03:50 -0300

    modal update

--- scripts.js
+++ scripts.js
@@ -1,17 +1,15 @@
+function openModal() {
+    console.log('Abrindo modal...');
+    videoModal.style.display = 'block';
+}
 document.addEventListener('DOMContentLoaded', function() {
     // Agora o código só será executado após o carregamento completo do DOM
-
+   
     // Seleciona a miniatura do vídeo e o botão de fechar
     const videoThumbnail = document.getElementById('videoThumbnail');
     const videoModal = document.getElementById('videoModal');
     const videoFrame = document.getElementById('videoFrame');
     const closeModalButton = document.getElementById('closeModal');
-
-    // Função para abrir o modal e carregar o vídeo
-    function openModal() {
-        console.log('Abrindo modal...');
-        videoModal.style.display = 'block';
-    }
 
     // Função para fechar o modal e parar o vídeo
     function closeModal() {
