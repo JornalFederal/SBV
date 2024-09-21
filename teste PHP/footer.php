129934ccbc69b96a5ca582bd9a9f83a4e2f424ac
unknown <jornalfederal.ifsp.sbv@gmail.com>
2024-09-21 18:30:08 -0300

    update2

--- teste PHP/footer.php
+++
@@ -1,5 +0,0 @@
-<footer>
-    <div class="container">
-        <p>&copy; 2024 Jornal Estudantil IFSP São João da Boa Vista. Todos os direitos reservados.</p>
-    </div>
-</footer>
--- teste PHP/scripts.js
+++
@@ -1,6 +0,0 @@
-// Precisa de Servidor e PHP para funcionar.
-document.getElementById('sugestaoForm').addEventListener('submit', function(event) {
-    event.preventDefault(); // Evita o envio do formulário
-    // Limpa o formulário
-    document.getElementById('sugestaoForm').reset();
-});
