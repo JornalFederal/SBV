e56ae82a6bb1d8d343f8b677f1f2c58bc938da93
unknown <jornalfederal.ifsp.sbv@gmail.com>
2024-09-21 19:13:20 -0300

    update arruma ID

--- scripts.js
+++ scripts.js
@@ -1,7 +1,3 @@
-function openModal() {
-    console.log('Abrindo modal...');
-    videoModal.style.display = 'block';
-}
 document.addEventListener('DOMContentLoaded', function() {
     // Agora o código só será executado após o carregamento completo do DOM
    
@@ -11,12 +7,18 @@ document.addEventListener('DOMContentLoaded', function() {
     const videoFrame = document.getElementById('videoFrame');
     const closeModalButton = document.getElementById('closeModal');
 
+    // Função para abrir o modal
+    function openModal() {
+        console.log('Abrindo modal...');
+        videoModal.style.display = 'block';
+    }
+
     // Função para fechar o modal e parar o vídeo
     function closeModal() {
         console.log('Fechando modal...');
         videoModal.style.display = 'none';
     }
-
+    
     // Adiciona o evento de clique na miniatura do vídeo
     videoThumbnail.addEventListener('click', openModal);
 
